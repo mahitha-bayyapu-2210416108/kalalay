484af1e241ec397f141628f9e4d0f78ff24e04bb
Mahitha <mahithabayyapu@gmail.com>
2025-06-05 06:58:18 -0500

    Initial commit

---
+++ backend/src/main/java/com/kalalay/backend/KalalayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kalalay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalalayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalalayBackendApplication.class, args);
+	}
+
+}
